9412de43da8bbb3e6408dfee49298f9b3242de8c
Ivaylo Yosifov <yosifov@gmail.com>
2018-05-02 19:55:37 +0300

    Finished but with bugs:
    1. No sound could be listened
    1. Cannot delete a record

--- TalkBoy/CreateSoundViewController.swift
+++ TalkBoy/CreateSoundViewController.swift
@@ -7,41 +7,139 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CreateSoundViewController: UIViewController {
 
 
     @IBOutlet weak var titleField: UITextField!
-    
     @IBOutlet weak var recordButton: UIButton!
-    
     @IBOutlet weak var addButton: UIButton!
-    
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Create Audio Session
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            //try? session.overrideOutputAudioPort(.speaker)
+            try? session.setActive(true)
+            session.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.audioURL = self.createNewDocumentPathFileName(fName: "audio.m4a")
+                        var recorderSettings : [String:Any] = [:]
+                        
+                        
+                        recorderSettings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+                        recorderSettings[AVSampleRateKey] = 12000
+                        recorderSettings[AVNumberOfChannelsKey] = 1
+                    
+                        self.audioRecorder = try? AVAudioRecorder(url: self.audioURL!, settings: recorderSettings)
+                        self.audioRecorder?.prepareToRecord()
+                        self.playButton.isEnabled = false
+                        self.addButton.isEnabled = false
+                        self.titleField.isEnabled = false
+                    } else {
+                        // failed to record!
+                        self.errorLabel.text = "The session was not allowed. Go back to the previous screen"
+                        self.errorLabel.isEnabled = true
+                    }
+                }
+            }
+            //URL to save audio
+            /*        if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+             let pathComponents = [basePath, "audio.m4a"]
+             if let audioURL = NSURL.fileURL(withPathComponents: pathComponents) {
+             */
+            
+            
+            //Create some settings
+            
+            
+            
+            //Create the audio recorder
+            
+            
+            
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
   
     
     @IBAction func recordTapped(_ sender: Any) {
+        if let audioRecorder = self.audioRecorder {
+            if audioRecorder.isRecording {
+                audioRecorder.stop()
+                recordButton.setTitle("Record", for: .normal)
+                playButton.isEnabled = true
+                addButton.isEnabled = true
+                titleField.isEnabled = true
+            } else {
+                audioRecorder.record()
+                recordButton.setTitle("Stop", for: .normal)
+                playButton.isEnabled = false
+                addButton.isEnabled = false
+                titleField.isEnabled = false
+            }
+            
+        }
     }
     
     
     @IBAction func addTapped(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+            let sound = Sound(entity: Sound.entity(), insertInto: context)
+            sound.title = titleField.text
+            sound.audioData = try? Data(contentsOf: audioURL!)
+            try? context.save()
+            navigationController?.popViewController(animated: true)
+            
+        }
     }
     
 
     @IBAction func playTapped(_ sender: Any) {
+        if let audioURL = self.audioURL {
+            let audioPlayer = try? AVAudioPlayer(contentsOf: audioURL)
+            audioPlayer?.play()
+        }
+        
     }
+    
+    func createNewDocumentPathFileName(fName: String )->URL{
+        
+        let dirPathNoScheme = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        
+        //add directory path file Scheme;  some operations fail w/out it
+        let dirPath = "file://\(dirPathNoScheme)"
+        //name your file, make sure you get the ext right .mp3/.wav/.m4a/.mov/.whatever
+        //let fileName = "thisIsYourFileName.mov"
+        let pathArray = [dirPath, fName]
+        let path = URL(string: pathArray.joined(separator: "/"))
+        
+        //use a guard since the result is an optional
+        guard let filePath = path else {
+            //if it fails do this stuff:
+            return URL(string: "choose how to handle error here")!
+        }
+        //if it works return the filePath
+        return filePath
+    }
+ 
+    
+   
     
     
     
--- TalkBoy/SoundsTableViewController.swift
+++ TalkBoy/SoundsTableViewController.swift
@@ -7,65 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundsTableViewController: UITableViewController {
 
+    var sounds : [Sound] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
+        }
+        
+    override func viewWillAppear(_ animated: Bool) {
+        getSounds()
+     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getSounds() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataStuff = try? context.fetch(Sound.fetchRequest()) as? [Sound] {
+                if let coreDataItems = coreDataStuff {
+                    sounds = coreDataItems
+                    tableView.reloadData()
+                }
+            }
+        }
     }
 
-    // MARK: - Table view data source
+   
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return sounds.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainReusableCell", for: indexPath)
+
+        let sound   = sounds[indexPath.row]
+        cell.textLabel?.text = sound.title
+
 
         return cell
     }
-    */
+ 
 
-    /*
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sound = sounds[indexPath.row]
+        let audioPlayer = try? AVAudioPlayer(data: sound.audioData!)
+        audioPlayer?.play()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+   
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+ 
+   
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let sound = sounds[indexPath.row]
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(sound)
+                getSounds()
+            }
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
